9966dc32203e63150468ec50ca87876546ab6714
Dhanya91298 <Ramadas.Dhanya@in.bosch.com>
2025-08-06 20:17:31 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -2,36 +2,42 @@
 from time import sleep
 import sys
 
-
+def loop_for_print():
+  for i in range(6):
+    print('\r* ', end='')
+    sys.stdout.flush()
+    sleep(1)
+    print('\r *', end='')
+    sys.stdout.flush()
+    sleep(1)
+  
+def range_check(value,minv,maxv):
+  retVal = False
+  if 'NA' not in minv or 'NA' not in maxv:
+    if value > maxv or value < minv:
+      loop_for_print()
+    else:
+       retVal = True
+  else:
+    if 'NA' in minv and value > maxv:
+        loop_for_print()
+    elif 'NA' in maxv and value<minv:
+         loop_for_print()
+    else:
+       retVal = True
+      
+  return retVal
+  
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+  rangeChecksMessages = {
+        "Temperature ": range_check(temperature,95, 102),
+        "pulseRate ": range_check(pulseRate, 20, 60),
+        "spo2 ": range_check(spo2, 90, 'NA')
+    }
+
+  for label,checkResult in rangeChecksMessages.items():
+        if not checkResult:
+            print(label + "out of range")
+    
+  return all(rangeChecksMessages.values())
+
