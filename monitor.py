6fadb78d57f9c53def3f51de76b12e2e0217f7a2
Dhanya91298 <Ramadas.Dhanya@in.bosch.com>
2025-08-07 09:05:00 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,35 +1,37 @@
-
 from time import sleep
 import sys
 
 def loop_for_print():
-  for i in range(6):
-    print('\r* ', end='')
-    sys.stdout.flush()
-    sleep(1)
-    print('\r *', end='')
-    sys.stdout.flush()
-    sleep(1)
-  
-def range_check(value,minv,maxv):
-    minv = None if minv == 'NA' else minv
-    maxv = None if maxv == 'NA' else maxv
+    for _ in range(6):
+        print('\r* ', end='')
+        sys.stdout.flush()
+        sleep(1)
+        print('\r *', end='')
+        sys.stdout.flush()
+        sleep(1)
 
-    if (minv is not None and value < minv) or (maxv is not None and value > maxv):
+def is_out_of_range(value, minv, maxv):
+    if minv != 'NA' and value < minv:
+        return True
+    if maxv != 'NA' and value > maxv:
+        return True
+    return False
+
+def range_check(value, minv, maxv):
+    if is_out_of_range(value, minv, maxv):
         loop_for_print()
         return False
     return True
-  
+
 def vitals_ok(temperature, pulseRate, spo2):
-  rangeChecksMessages = {
-        "Temperature ": range_check(temperature,95, 102),
-        "pulseRate ": range_check(pulseRate, 20, 60),
-        "spo2 ": range_check(spo2, 90, 'NA')
+    checks = {
+        "Temperature": range_check(temperature, 95, 102),
+        "Pulse Rate": range_check(pulseRate, 20, 60),
+        "SpO2": range_check(spo2, 90, 'NA')
     }
 
-  for label,checkResult in rangeChecksMessages.items():
-        if not checkResult:
-            print(label + "out of range")
-    
-  return all(rangeChecksMessages.values())
+    for label, result in checks.items():
+        if not result:
+            print(f"{label} out of range")
 
+    return all(checks.values())
