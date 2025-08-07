00c9b6319e3370b53ba5e662cfb625f598e009fb
Dhanya91298 <Ramadas.Dhanya@in.bosch.com>
2025-08-07 09:32:21 +0530

    Update monitor.test.py

--- monitor.test.py
+++ monitor.test.py
@@ -10,11 +10,11 @@ class MonitorTest(unittest.TestCase):
         self.assertFalse(vitals_ok(99, 105, 70))
         self.assertTrue(vitals_ok(98.1, 70, 98))
         self.assertTrue(vitals_ok(100,80,100))
-        self.assertFalse(vitals_ok(90, 80, 100))
 
 
 if __name__ == '__main__':
   unittest.main()
+
 
 
 
