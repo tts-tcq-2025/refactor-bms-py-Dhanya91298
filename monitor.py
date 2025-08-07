2c51b7a0b13327fa30bccefb6f0200ebb38c8033
Dhanya91298 <Ramadas.Dhanya@in.bosch.com>
2025-08-07 09:35:13 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,8 +1,8 @@
 from time import sleep
 import sys
 
-def loop_for_print():
-    for _ in range(6):
+def loop_for_print_when_out_of_range():
+    for i in range(6):
         print('\r* ', end='')
         sys.stdout.flush()
         sleep(1)
@@ -19,17 +19,17 @@ def is_above_max(value, maxv):
 def is_out_of_range(value, minv, maxv):
     return is_below_min(value, minv) or is_above_max(value, maxv)
 
-def range_check(value, minv, maxv):
+def range_validation(value, minv, maxv):
     if is_out_of_range(value, minv, maxv):
-        loop_for_print()
+        loop_for_print_when_out_of_range()
         return False
     return True
 
 def vitals_ok(temperature, pulseRate, spo2):
     checks = {
-        "Temperature": range_check(temperature, 95, 102),
-        "Pulse Rate": range_check(pulseRate, 60, 100),
-        "SpO2": range_check(spo2, 90, 'NA')
+        "Temperature": range_validation(temperature, 95, 102),
+        "Pulse Rate": range_validation(pulseRate, 60, 100),
+        "SpO2": range_validation(spo2, 90, 'NA')
     }
 
     for label, result in checks.items():
